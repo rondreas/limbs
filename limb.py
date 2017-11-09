50b3f117690dfa77a136e96dd8c04a33e0c12837
Andreas Rånman <andreas.ranman@gmail.com>
2017-11-09 18:48:03 +0100

    Finished Soft IK
    Finished my solution to soft IK and found another one on the interwebs. Going to finish second one too and compare results.

--- limb.py
+++ limb.py
@@ -45,6 +45,7 @@ class Limb(object):
 
     def __init__(self, name, startJoint=None, endJoint=None, parent=None):
         self.name = name
+        self.parent = parent
 
         selection = pm.ls(sl=True, type="joint")
         if startJoint and endJoint:
@@ -252,7 +253,7 @@ class IKLimb(Limb):
 
         pm.poleVectorConstraint(pvcLoc, ikHandle)
 
-        pm.parent(ikHandle, controller)
+        #pm.parent(ikHandle, controller)
 
     def slide(self):
         """ """
@@ -261,13 +262,16 @@ class IKLimb(Limb):
     def soft(self, controller):
         """ Create node network to set soft IK for limb."""
 
+        # TODO: Review naming, and see if nodes could be removed.
+
         length = self.length()
 
         # Add attribute to our controller for soft IK damp factor.
         pm.addAttr(
             controller,
-            shortName='damp',
-            longName='Dampening',
+            shortName='sf',
+            longName='soft',
+            niceName='Soft',
             defaultValue=0.0,
             maxValue=1.0,
             minValue=0.0,
@@ -277,31 +281,229 @@ class IKLimb(Limb):
         )
 
         # Decompose Matrices to get World Space Position for target and start of limb, then supply a distance node.
-        target = pm.createNode('decomposeMatrix', name='{}_target_WMtx')
-        start = pm.createNode('decomposeMatrix', name='{}_WMtx')
-        targetDistance = pm.createNode('distanceBetween', name='{}_targetDistance')
+        translate_output = pm.group(empty=True, name='{}_translate_output'.format(self.name))
+        pm.parent(translate_output, self.joints[0], relative=True)
+        pm.parent(translate_output, self.joints[0].listRelatives(parent=True)[0].nodeName())
 
+        start = pm.createNode('decomposeMatrix', name='{}_start_WMtx'.format(self.name))
+        target = pm.createNode('decomposeMatrix', name='{}_target_WMtx'.format(self.name))
+
+        targetDistance = pm.createNode('distanceBetween', name='{}_targetDistance'.format(self.name))
+
+        # Connect sources to our decompose matrices
         pm.connectAttr(controller.worldMatrix[0], target.inputMatrix)
-        pm.connectAttr(self.joints[0].worldMatrix[0], start.inputMatrix)
+        pm.connectAttr(translate_output.worldMatrix[0], start.inputMatrix)
 
         # Hook the positions to the distance node, order shouldn't matter
         pm.connectAttr(target.outputTranslate, targetDistance.point1)
         pm.connectAttr(start.outputTranslate, targetDistance.point2)
 
         # Nodes for Soft IK solution.
-        softDist = pm.createNode('multiplyDivide', name='{}_softDistance')
-        softDist.setAttr('operation', 1)    # Set to Multiply
-        softDist.setAttr('input2X', length)
-        pm.connectAttr(controller.damp, softDist.input1X)
+        soft_distance = pm.createNode('multiplyDivide', name='{}_softDist'.format(self.name))
+        soft_distance.setAttr('operation', 1)    # Set to Multiply
+        soft_distance.setAttr('input2X', length)
+        pm.connectAttr(controller.soft, soft_distance.input1X)
 
         # Hard Distance is the range we want IK to follow fully, which is total length - distance we want soft IK.
-        hardDist = pm.createNode('plusMinusAverage', name='{}_hardDistance')
-        hardDist.setAttr('operation', 2)    # Set to Subtract
-        hardDist.setAttr('input1D[0]', length)
+        hard_distance = pm.createNode('plusMinusAverage', name='{}_hardDist'.format(self.name))
+        hard_distance.setAttr('operation', 2)    # Set to Subtract
+        hard_distance.setAttr('input1D[0]', length)
+        pm.connectAttr(soft_distance.outputX, hard_distance.input1D[1])
 
-        finalDist = pm.createNode('condition', name='{}_finalDistance')
-        finalDist.setAttr('operation', 5)
+        # Get the Hard distance minus distance to target.
+        subtract_distance = pm.createNode('plusMinusAverage', name='{}_subDist'.format(self.name))
+        subtract_distance.setAttr('operation', 2)   # Subtract
+        pm.connectAttr(hard_distance.output1D, subtract_distance.input1D[0])
+        pm.connectAttr(targetDistance.distance, subtract_distance.input1D[1])
+
+        distance_factor = pm.createNode('multiplyDivide', name='{}_distFac'.format(self.name))
+        distance_factor.setAttr('operation', 2) # Divide
+        pm.connectAttr(subtract_distance.output1D, distance_factor.input1X)
+        pm.connectAttr(soft_distance.outputX, distance_factor.input2X)
+
+        # To avoid divide by zero errors we also connect a condition to freeze division node.
+        freeze_division = pm.createNode('condition', name='{}_freezeDivision'.format(self.name))
+        freeze_division.setAttr('colorIfTrueR', 1)
+        freeze_division.setAttr('colorIfFalseR', 0)
+        freeze_division.setAttr('operation', 0)  # Equals
+        freeze_division.setAttr('secondTerm', 0)
+        pm.connectAttr(controller.soft, freeze_division.firstTerm)
+        pm.connectAttr(freeze_division.outColorR, distance_factor.frozen)
+
+        # Exponential function for our distance factor
+        exp = pm.createNode('multiplyDivide', name='{}_exp'.format(self.name))
+        exp.setAttr('operation', 3)  # Set to Power
+        exp.setAttr('input1X', math.e)
+        pm.connectAttr(distance_factor.outputX, exp.input2X)
+
+        # 1 - result of exponential function.
+        one_minus = pm.createNode('plusMinusAverage', name='{}_one_minus'.format(self.name))
+        one_minus.setAttr('operation', 2)
+        one_minus.setAttr('input1D[0]', 1)
+        pm.connectAttr(exp.outputX, one_minus.input1D[1])
+
+        # Multiply Soft Distance with the result of the exponential function.
+        mult_soft_distance = pm.createNode('multiplyDivide', name='{}_multSoftExp'.format(self.name))
+        mult_soft_distance.setAttr('operation', 1)  # Multiply
+        pm.connectAttr(soft_distance.outputX, mult_soft_distance.input1X)
+        pm.connectAttr(one_minus.output1D, mult_soft_distance.input2X)
+
+        # Sum Hard Distance with whatever one can call the node above.
+        final_soft_distance = pm.createNode('plusMinusAverage', name='{}_sum_softDistance'.format(self.name))
+        final_soft_distance.setAttr('operation', 1) # Sum
+        pm.connectAttr(hard_distance.output1D, final_soft_distance.input1D[0])
+        pm.connectAttr(mult_soft_distance.outputX, final_soft_distance.input1D[1])
+
+        # If Soft distance is greater than length.
+        finalDist = pm.createNode('condition', name='{}_finalDistance'.format(self.name))
+        finalDist.setAttr('operation', 5)  # Less or Equal
         pm.connectAttr(targetDistance.distance, finalDist.firstTerm)
-        pm.connectAttr(hardDist.output1D, finalDist.secondTerm)
+        pm.connectAttr(hard_distance.output1D, finalDist.secondTerm)
+        pm.connectAttr(targetDistance.distance, finalDist.colorIfTrueR)
+        pm.connectAttr(final_soft_distance.output1D, finalDist.colorIfFalseR)
 
-        pm.createNode('condition', name='{}_finalRatio')
+        soft_ratio = pm.createNode('multiplyDivide', name='{}_soft_ratio'.format(self.name))
+        soft_ratio.setAttr('operation', 2)  # Multiply
+        pm.connectAttr(finalDist.outColorR, soft_ratio.input1X)
+        pm.connectAttr(targetDistance.distance, soft_ratio.input2X)
+
+        # If soft ratio is not 0 soft ratio, else 1.
+        ratio = pm.createNode('condition', name='{}_finalRatio'.format(self.name))
+        ratio.setAttr('secondTerm', 0)
+        ratio.setAttr('colorIfTrueR', 1)
+        ratio.setAttr('operation', 0) # Equals
+        pm.connectAttr(controller.soft, ratio.firstTerm)
+        pm.connectAttr(soft_ratio.outputX, ratio.colorIfFalseR)
+
+        # 1 - ratio to get what to multiply vectors with.
+        subtract_ratio = pm.createNode('plusMinusAverage', name='{}_oneMinus_ratio'.format(self.name))
+        subtract_ratio.setAttr('operation', 2)   # Subtract
+        subtract_ratio.setAttr('input1D[0]', 1)
+        pm.connectAttr(ratio.outColorR, subtract_ratio.input1D[1])
+
+        # Start Vector * final ratio
+        start_vector = pm.createNode('multiplyDivide', name='{}_sVector'.format(self.name))
+        start_vector.setAttr('operation', 1)  # Multiply
+        pm.connectAttr(start.outputTranslate, start_vector.input1)
+        pm.connectAttr(subtract_ratio.output1D, start_vector.input2X)
+        pm.connectAttr(subtract_ratio.output1D, start_vector.input2Y)
+        pm.connectAttr(subtract_ratio.output1D, start_vector.input2Z)
+
+        # Target Vector * final ratio
+        target_vector = pm.createNode('multiplyDivide', name='{}_eVector'.format(self.name))
+        target_vector.setAttr('operation', 1)  # Multiply
+        pm.connectAttr(target.outputTranslate, target_vector.input1)
+        pm.connectAttr(ratio.outColorR, target_vector.input2X)
+        pm.connectAttr(ratio.outColorR, target_vector.input2Y)
+        pm.connectAttr(ratio.outColorR, target_vector.input2Z)
+
+        # Final World Position, sum of altered vectors.
+        final_position = pm.createNode('plusMinusAverage', name='{}_sum_vectors'.format(self.name))
+        final_position.setAttr('operation', 1)  # Sum
+        pm.connectAttr(start_vector.output, final_position.input3D[0])
+        pm.connectAttr(target_vector.output, final_position.input3D[1])
+
+        # Compose Matrix
+        compose_mtx = pm.createNode('composeMatrix', name='{}_final_ws_mtx'.format(self.name))
+        pm.connectAttr(final_position.output3D, compose_mtx.inputTranslate)
+
+        # Matrix Multiplication Final *
+        multiply_mtx = pm.createNode('multMatrix', name='{}_cnst_mtx'.format(self.name))
+        pm.connectAttr(compose_mtx.outputMatrix, multiply_mtx.matrixIn[0])
+
+        # Create a null Transform object which we will apply resulting matrix onto.
+        driven = pm.group(empty=True, name='{}_driven_srtBuffer'.format(self.name))
+        pm.parent(driven, controller, relative=True)
+        pm.parent(driven, world=True)
+        pm.connectAttr(driven.parentInverseMatrix[0], multiply_mtx.matrixIn[1])
+
+        # Driven decomposed matrix
+        result_mtx = pm.createNode('decomposeMatrix', name='{}_offset_mtx'.format(self.name))
+        pm.connectAttr(multiply_mtx.matrixSum, result_mtx.inputMatrix)
+        pm.connectAttr(result_mtx.outputTranslate, driven.translate)
+
+    def h_soft(self, controller):
+        """ Soft IK as show on http://hhoughton07.wixsite.com/hazmondo/maya-ik-arm """
+
+        # TODO not finished!
+
+        """ Parent a Loc to top joint, use same parent as joint. 
+        Get ws pos matrix for loc, vector loc->ctrl. Output soft will be magnitude for vector and applied to IK handle."""
+
+        # Inputs:
+        #   CTRL.Soft
+        #   Limb.length
+        #   Distance to CTRL
+
+        # Outliner example
+        # Component
+        #   parent
+        #       loc
+        #       joints...
+        #   ctrl
+        #   ikHandle
+
+        length = self.length()
+
+        pm.addAttr(
+            controller,
+            shortName='sf',
+            longName='soft',
+            niceName='Soft',
+            defaultValue=0.0,
+            maxValue=1.0,
+            minValue=0.0,
+            hidden=False,
+            keyable=True,
+            readable=True,
+        )
+
+        # Create a locator, position at first joint in world, and set parent to same as for the joint.
+        start = pm.spaceLocator(name='{}_softIK_start'.format(self.name))
+        pm.parent(
+            start,
+            self.joints[0].listRelatives(parent=True),
+            relative=True,
+        )
+
+        target = pm.createNode('decomposeMatrix', name='{}_target_WMtx')
+        pm.connectAttr(controller.worldMatrix[0], target.inputMatrix)
+
+        distance = pm.createNode('distanceBetween', name='{}_targetDistance')
+
+        # Hook the positions to the distance node, order shouldn't matter
+        pm.connectAttr(target.outputTranslate, distance.point1)
+        pm.connectAttr(start.worldPosition[0], distance.point2)
+
+        # Get Limb.length() minus controller.soft
+        len = pm.createNode('plusMinusAverage', name='minLen')
+        len.setAttr('operation', 2)
+        len.setAttr('input1D[0]', length)
+        pm.connectAttr(controller.soft, len.input1D[1])
+
+        # Take the distance minus Soft attribute.
+        dist = pm.createNode('plusMinusAverage', name='minDist')
+
+        div = pm.createNode('multiplyDivide', name='divSoft_{}')
+        div.setAttr('operation', 2)  # Set to Divide
+
+        inv = pm.createNode('multiplyDivide', name='inv_')
+        inv.setAttr('operation', 1) # Set to Multiply
+        inv.setAttr('input1X', -1)
+        pm.connectAttr(div.output, inv.input2)  # Only X channel carrying data.
+
+        exp = pm.createNode('multiplyDivide', name='exp_{}'.format(self.name))
+        exp.setAttr('operation', 3) # Set to Power
+        exp.setAttr('input1X', math.e)
+        pm.connectAttr(inv.output, exp.input2)
+
+        mul = pm.createNode('multiplyDivide', name='multSoft')
+        mul.setAttr('operation', 1) # Set to Multiply
+
+        magnitude = pm.createNode('plusMinusAverage', name='minSoft')
+
+        # If soft, soft factor, else limb length
+        soft = pm.createNode('condition')
+
+        # If distance less than length,
+        final_magnitude = pm.createNode('condition')
