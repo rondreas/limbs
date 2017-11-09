38ca8f3be699633b41ba1804d5f8705e827b9e32
Andreas Rånman <andreas.ranman@gmail.com>
2017-11-09 10:21:31 +0100

    Created Line object
    Wanted to check distance to a point from a known line and couldn't find method for this in pymel or openmaya api so made one myself.

--- utils/vectors.py
+++ utils/vectors.py
@@ -11,3 +11,15 @@ def sum_distance(points):
     for p in range(len(points) - 1):
         totalDistance += points[p].distanceTo(points[p+1])
     return totalDistance
+
+class Line(object):
+    def __init__(self, pointA, pointB):
+        self.a = om.MPoint(pointA)
+        self.b = om.MPoint(pointB)
+
+    def distance_to(self, point):
+        # Make point into a Maya point object
+        p = om.MPoint(point)
+        # Calculate distance d from line to point p
+        d = (om.MVector(p - self.a) ^ om.MVector(p - self.b)).length() / om.MVector(self.b - self.a).length()
+        return d
